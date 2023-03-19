8f4696380e9ed967099e816f28bf4c35c1cbdfc3
Cole <coleportolesi@gmail.com>
2023-03-19 18:42:41 -0400

    roomba ai

--- Assets/Scripts/Roomba.cs
+++ Assets/Scripts/Roomba.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Roomba : MonoBehaviour
-{
+public class Roomba : MonoBehaviour {
+
+    [SerializeField]
+    Transform[] waypoints;
+
+    [SerializeField]
+    float moveSpeed = 2f;
+    int waypointIndex = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = waypoints[waypointIndex].transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
         
+        if(transform.position == waypoints [waypointIndex].transform.position)
+        {
+            waypointIndex += 1;
+        }
+        if(waypointIndex == waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("GameOver"); 
+        }
     }
 }
