4679ae29a1fefadf156c757539a73051e531130d
IBinooI <paulr70@hotmail.com>
2023-03-19 18:23:02 -0400

    Main menu done

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,11 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame(){
+<<<<<<< Updated upstream
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+=======
+        SceneManager.LoadScene("Tanman");
+>>>>>>> Stashed changes
     }
     public void QuitGame(){
         Application.Quit();
